7c6a136dc30f01dd590583b0d86f749f2b93489b
Val-d-emar <vhot@live.ru>
2026-04-22 21:00:16 +0400

    feat: add cloudfront

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 import * as path from "path";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,27 +13,41 @@ export class CdkStack extends cdk.Stack {
     // Creating an S3 bucket for hosting
     const bucket = new s3.Bucket(this, "MyStoreBucket", {
       // Setting up the bucket as a website
-      websiteIndexDocument: "index.html",
+      //websiteIndexDocument: "index.html",
       // Setting up automatic bucket deletion when deleting the stack
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       // Automatic file cleanup before deleting the bucket
       autoDeleteObjects: true,
-      // Enable public read access
-      publicReadAccess: true,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS_ONLY,
+      // Disable public read access
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
     });
 
-    // Setting up automatic file uploads from the folder dist
+    const distribution = new cloudfront.Distribution(
+      this,
+      "MyStoreDistribution",
+      {
+        defaultBehavior: {
+          origin: new origins.S3StaticWebsiteOrigin(bucket),
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+        defaultRootObject: "index.html", // This setting will automatically create an OAC and update the Bucket Policy
+      },
+    );
+
+    // Updating deployment: adding binding to CloudFront for auto-invalidation
     new s3deploy.BucketDeployment(this, "DeployWebsite", {
       sources: [
         s3deploy.Source.asset(path.join(__dirname, "..", "..", "dist")),
       ],
       destinationBucket: bucket,
+      distribution, 
+      distributionPaths: ["/*"], // We clear the cache for all files with each deploy
     });
 
     // Print the URL of the website to the console after the deployment is complete
-    new cdk.CfnOutput(this, "BucketWebsiteUrl", {
-      value: bucket.bucketWebsiteUrl,
+    new cdk.CfnOutput(this, "CloudFrontUrl", {
+      value: distribution.distributionDomainName,
     });
   }
 }
