0705e7124e45e49312d802a988c872e8c386a97f
Val-d-emar <vhot@live.ru>
2026-04-22 19:43:39 +0400

    feat: write CdkStack.ts

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,12 +1,35 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Write your code here
+    // Creating an S3 bucket for hosting
+    const bucket = new s3.Bucket(this, "MyStoreBucket", {
+      // Setting up the bucket as a website
+      websiteIndexDocument: "index.html",
+      // Setting up automatic bucket deletion when deleting the stack
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      // Automatic file cleanup before deleting the bucket
+      autoDeleteObjects: true,
+      // Enable public read access
+      publicReadAccess: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS_ONLY,
+    });
+
+    // Setting up automatic file uploads from the folder dist
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset('../dist')],
+      destinationBucket: bucket,
+    });
+
+    // Print the URL of the website to the console after the deployment is complete
+    new cdk.CfnOutput(this, 'BucketWebsiteUrl', {
+      value: bucket.bucketWebsiteUrl,
+    });
 
   }
 }
