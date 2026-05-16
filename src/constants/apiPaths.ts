2d7b6f256f52b1d2fcc910a2075e301866f90731
Val-d-emar <vhot@live.ru>
2026-05-16 16:30:56 +0400

    fix: apiPaths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://jco1jj7ev7.execute-api.eu-north-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://pko6smj112.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://jco1jj7ev7.execute-api.eu-north-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
