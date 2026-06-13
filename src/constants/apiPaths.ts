38f239e1151e997bc6d74c70cc59bc77f25e0234
Val-d-emar <vhot@live.ru>
2026-06-13 18:33:42 +0400

    fix: order path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://jco1jj7ev7.execute-api.eu-north-1.amazonaws.com/prod",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://4nyjlii0ai.execute-api.eu-north-1.amazonaws.com/prod/api/profile/cart",
   import: "https://pko6smj112.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://jco1jj7ev7.execute-api.eu-north-1.amazonaws.com/prod",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://4nyjlii0ai.execute-api.eu-north-1.amazonaws.com/prod/api",
 };
 
 export default API_PATHS;
