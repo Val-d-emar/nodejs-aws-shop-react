090903a9fd03a892ed290cc080796eff285307e8
Val-d-emar <vhot@live.ru>
2026-06-20 14:15:56 +0400

    fix: add cloudfront for EB (CORS fix)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://jco1jj7ev7.execute-api.eu-north-1.amazonaws.com/prod",
-  order: "https://4nyjlii0ai.execute-api.eu-north-1.amazonaws.com/prod/api/profile/cart",
+  order: "https://d1nht5hp218c9h.cloudfront.net/api/profile/cart",
   import: "https://pko6smj112.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://jco1jj7ev7.execute-api.eu-north-1.amazonaws.com/prod",
-  cart: "https://4nyjlii0ai.execute-api.eu-north-1.amazonaws.com/prod/api",
+  cart: "https://d1nht5hp218c9h.cloudfront.net/api",
 };
 
 export default API_PATHS;
