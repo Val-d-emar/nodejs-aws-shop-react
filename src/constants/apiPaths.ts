eb6ade833c33ce5bbd62e597991b98e8a4d5f5b1
Val-d-emar <vhot@live.ru>
2026-06-21 14:19:09 +0400

    feat: add bff links

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://jco1jj7ev7.execute-api.eu-north-1.amazonaws.com/prod",
-  order: "https://d1nht5hp218c9h.cloudfront.net/api/profile/cart",
+  product: "https://d2azifyt8opqc6.cloudfront.net/product",
+  order: "https://d2azifyt8opqc6.cloudfront.net/cart/profile/cart",
   import: "https://pko6smj112.execute-api.eu-north-1.amazonaws.com/prod",
-  bff: "https://jco1jj7ev7.execute-api.eu-north-1.amazonaws.com/prod",
-  cart: "https://d1nht5hp218c9h.cloudfront.net/api",
+  bff: "https://d2azifyt8opqc6.cloudfront.net/product",
+  cart: "https://d2azifyt8opqc6.cloudfront.net/product/cart",
 };
 
 export default API_PATHS;
