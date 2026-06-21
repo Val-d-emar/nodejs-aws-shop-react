e9c941b188a86bdf3ee44868b08c4a766de0b1bf
Val-d-emar <vhot@live.ru>
2026-06-21 16:13:48 +0400

    fix: apiPaths, works

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://d2azifyt8opqc6.cloudfront.net/product",
-  order: "https://d2azifyt8opqc6.cloudfront.net/cart/profile/cart",
+  order: "https://d2azifyt8opqc6.cloudfront.net/cart/api/profile/cart",
   import: "https://pko6smj112.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://d2azifyt8opqc6.cloudfront.net/product",
-  cart: "https://d2azifyt8opqc6.cloudfront.net/product/cart",
+  cart: "https://d2azifyt8opqc6.cloudfront.net/cart/api",
 };
 
 export default API_PATHS;
