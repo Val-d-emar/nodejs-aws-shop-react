e569bff0c534a351edb91cc82a69d26c2263e6eb
Val-d-emar <vhot@live.ru>
2026-06-22 00:15:39 +0400

    fix: paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://d2azifyt8opqc6.cloudfront.net/product",
   order: "https://d2azifyt8opqc6.cloudfront.net/cart/api/profile/cart",
-  import: "https://pko6smj112.execute-api.eu-north-1.amazonaws.com/prod",
+  import: "https://d2azifyt8opqc6.cloudfront.net/import",
   bff: "https://d2azifyt8opqc6.cloudfront.net/product",
   cart: "https://d2azifyt8opqc6.cloudfront.net/cart/api",
 };
